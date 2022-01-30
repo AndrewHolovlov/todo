e29dcfc225361f1aa60607903c6e15afb2223f50
andrew <a.holovlov@zd.ua>
2022-01-30 23:09:23 +0200

    Add README.md

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, Executor, Attachment
+
+admin.site.register(Task)
+admin.site.register(Executor)
+admin.site.register(Attachment)
